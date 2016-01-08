05ed08d35c1007a2e55c9814c077e09514f03d1e
nagaswarup43 <nagaswarup+de@dhruvsoft.com>
2016-01-08 15:43:27 +0530

    dfdfd

---
+++ change-pwd.php
@@ -0,0 +1,104 @@
+<?php
+@session_start();
+include_once('php/init.php');
+include_once('php/config.php');
+global $db;
+$status = (isset($_GET['s']))?$_GET['s']: 0;
+$is_admin = (isset($_GET['a']))?$_GET['a']: 0;
+$is_new = (isset($_GET['n']))?$_GET['n']: 0;
+$id = (isset($_GET['id']))?base64_decode($_GET['id']): 0;
+
+$title = "Change My Password";
+if($status){
+     $title = "Set Password"; 
+    login_check(0);
+    @pg_update("contacts", array("status" => 1), array("pwd_activation_code" =>$id));
+    $user_query = @pg_query($db,"SELECT id,name "
+            . " FROM contacts"
+            . " WHERE pwd_activation_code='".$id."'");
+    $res = @pg_fetch_assoc($user_query);
+    $id = $res['id'];
+    if(is_bool($res) || is_null($res))
+    {
+        header("Location:index.php");
+    }
+}else{
+    
+    login_check(1);
+}
+
+include_once('head-first.php');
+include_once('header.php');
+ $user_id = (isset($_SESSION['user']['id']))?$_SESSION['user']['id']:$id;
+ 
+ $result= get_settings();
+if(is_array($result) && count($result)>0)
+{
+  $max_pass_len = $result['max_pass_len'] ;
+  $min_pass_len = $result['min_pass_len'] ;
+}
+?>
+<input type="hidden" id="page" value="<?php echo $title ?>">
+<input type="hidden" id="minPassLen" value="<?php echo $min_pass_len ?>">
+<!--<input type="hidden" id="maxPassLen" value="<?php echo $max_pass_len ?>">-->
+  <div class="row toggle-full-width">
+    <div class="large-12 columns">
+      <input type="hidden" id="id" value="<?php echo base64_encode($user_id) ?>" >
+      <input type="hidden" id="isAdmin" value="<?php echo $is_admin?>" >
+      <h4><br><?php echo $title; ?></h4>
+       <h4 class="right"><small><em>* = Required</em></small></h4>
+      <hr>
+      <input type="hidden" id="status" value="<?php echo $status ?>" >
+      <div id="section_1">
+          <?php if(!$status) { ?>
+           <div class="row">
+          <div class="medium-6 columns">
+            <label for="oldPwd">
+               
+                <strong data-tooltip aria-haspopup="true" class="fi-info has-tip radius" title="Enter Old Password.&lt;br&gt;&lt;small&gt;&lt;em&gt;This field is required.&lt;/em&gt;&lt;/small&gt;"> 
+                  Old Password*</strong>
+                <input type="password" maxlength="<?php echo $max_pass_len ?>" id="oldPwd" name="oldPwd" placeholder="Old Password" class="radius" required>
+              <span class="label error alert  radius" style="display:none">Required</span>
+            </label>
+          </div>
+          </div>
+          <?php } ?>
+          <div class="row">
+          <div class="medium-6 columns">
+            <label for="newPwd">
+                
+                <strong data-tooltip aria-haspopup="true" class="fi-info has-tip radius" title="Enter New Password.&lt;br&gt;&lt;small&gt;&lt;em&gt;This field is required.&lt;/em&gt;&lt;/small&gt;"> 
+                  New Password*</strong>
+                <input type="password" maxlength="<?php echo $max_pass_len ?>" id="newPwd" name="newPwd" placeholder="New Password" class="radius" required>
+              <span class="label error alert  radius" style="display:none">Required</span>
+               <span class="correctPassword" style="display:none" ><img class="imageShown" src="img/correct1.png"  ></span>
+            </label>
+          </div>
+          </div>
+          
+           <div class="row">
+          <div class="medium-6 columns">
+            <label for="confirmPwd">
+                
+                <strong data-tooltip aria-haspopup="true" class="fi-info has-tip radius" title="Enter Confirm New Password.&lt;br&gt;&lt;small&gt;&lt;em&gt;This field is required.&lt;/em&gt;&lt;/small&gt;"> 
+                 Confirm New Password*</strong>
+                <input type="password" id="confirmPwd" name="confirmPwd" placeholder="Confirm New Password" class="radius" maxlength="<?php echo $max_pass_len ?>" >
+              <span class="label error alert radius" style="display:none">Required</span>
+              <span class="correctPassword" style="display:none" ><img class="imageShown" src="img/correct1.png"  ></span>
+            </label>
+          </div>
+          </div>
+          
+          <div class="clear" style="height:15px"></div>
+          <div class="row">
+          <div class="columns small-12"> 
+          
+            <a  id="changePwdSubmit" class="button radius submit link">Submit</a> <br>
+          </div>
+        </div>
+     
+      </div>
+    </div>
+  </div>
+<?php include('footer.php');?>
+

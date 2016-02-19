5914b40875a29e4f445c097f8bc52a06e139e414
rushyendra <rushyendra@dhruvsoft.com>
2016-02-19 13:07:35 +0530

    change pwd

--- wp-content/themes/portal/ajax/loginmanage/change-pwd-submit.php
+++ wp-content/themes/portal/ajax/loginmanage/change-pwd-submit.php
@@ -33,7 +33,7 @@ $user_email=$rel->user_email;
 $result = $wpdb->get_row("SELECT ID,user_pass,user_nicename,user_email,user_login,is_Deactive 
 				FROM " . $table_prefix . "users
 				WHERE ID='" . $id . "'");
-$user_email=$result->user_email;
+echo $user_email=$result->user_email;
 //var_dump($result);
 //echo wp_check_password( $old_pwd, $result->user_pass,$id);	
 $is_fun = 0;
@@ -56,6 +56,7 @@ if ($status == 1) {
     }
 }
 if ($is_fun) {
+    
     wp_set_password($new_pwd,$id);
     session_start();
     $_SESSION['msg'] = 'Your Password is Changed Successfully';
@@ -86,19 +87,14 @@ if ($is_fun) {
     $message = str_replace("!!userName!!", $user_nicename, $message);
     $message = str_replace("!!uname!!", $result->user_email, $message);
     $message = str_replace("!!pwd!!", $new_pwd, $message);
-   // var_dump($message);
-    // if($type != "")  
-    //$_SESSION['msg'] = $subject;
-
+  
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     $headers .= 'From: ' . $admin_email . "\r\n";
 
-    mail($user_email,$subject,nl2br($message),$headers);
-  //  var_dump($user_email);
-   // echo $uner_email;
 
-  
+    @mail($user_email,$subject,nl2br($message),$headers);
+   
     echo "1";
 }
 ?>

4d6a8c50405d1fc7b42b1d36fb51f686569eb164
rushyendra <rushyendra@dhruvsoft.com>
2016-02-24 12:14:35 +0530

    View Attachments

--- wp-config.php
+++ wp-config.php
@@ -27,7 +27,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:3307');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
